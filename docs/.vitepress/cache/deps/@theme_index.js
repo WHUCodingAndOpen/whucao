c7c6c5bee38273c2f9054b8161775b38304fea29
EpicMo <1982742309@qq.com>
2023-05-19 22:48:18 +0800

    init

---
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -0,0 +1,41 @@
+// .yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/index.js
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+
+// .yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/without-fonts.js
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "F:/Netife/netife_docs/.yarn/cache/vitepress-npm-1.0.0-alpha.75-665946820d-eb146a6547.zip/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+var theme = {
+  Layout,
+  enhanceApp: ({ app }) => {
+    app.component("Badge", VPBadge);
+  }
+};
+var without_fonts_default = theme;
+export {
+  default5 as VPDocAsideSponsors,
+  default3 as VPHomeFeatures,
+  default2 as VPHomeHero,
+  default4 as VPHomeSponsors,
+  default9 as VPTeamMembers,
+  default6 as VPTeamPage,
+  default8 as VPTeamPageSection,
+  default7 as VPTeamPageTitle,
+  without_fonts_default as default
+};
+//# sourceMappingURL=@theme_index.js.map
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'WHUCodingAndOpen',
+  description: 'WHU编程交流兼开源社区',
+  base: "/WHUCAO/",
+  lastUpdated: true,
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" }, 
+    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/WHUCodingAndOpen" }],
+    footer: {
+      message: "Released under the GPLV3 License.",
+      copyright: "Copyright © 2023",
+    },
+    sidebar: {
+    },
+  },
+}
